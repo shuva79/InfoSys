a303fb83f360f27806c9238b5c8fa662b2b34ceb
shuva79 <shuvabro@gmail.com>
2023-01-08 14:50:26 +0545

    Added footer and home page content

--- coursework/Main/main.js
+++ coursework/Main/main.js
@@ -7,3 +7,11 @@ this.window.addEventListener("scroll", function(){
     h1.style.filter = 'opacity(' + (1 - ((totalHeight-scrollTop)/totalHeight)) * 100 + '%)'; //opacity reduces on scrolling
 });
 
+this.window.addEventListener("scroll", function(){
+    let h1 = document.getElementById('text_content');     //we take element's id and store its in a variable
+    
+    let scrollTop = window.pageYOffset;  //measures how much we've scrolled vertically in pixels
+    let totalHeight = window.innerHeight;   //inner height means the actual height of the actual webpage
+    
+    h1.style.filter = 'opacity(' + (1 - ((totalHeight-scrollTop)/totalHeight)) * 100 + '%)'; //opacity reduces on scrolling
+});
